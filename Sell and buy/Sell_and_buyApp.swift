4c522944251da280d334536d99113e84890c5404
Rohullah Haideri <rohullahhaideri@gmail.com>
2022-10-28 15:15:23 +0200

    Initial Commit

---
+++ Sell and buy/Sell_and_buyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sell_and_buyApp.swift
+//  Sell and buy
+//
+//  Created by Rohullah Haideri on 2022-10-28.
+//
+
+import SwiftUI
+
+@main
+struct Sell_and_buyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
